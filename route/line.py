ff79460a9650f72d354b9ae1d706c4f179b5d684
sadano111 <101854082+sadano111@users.noreply.github.com>
2024-03-30 20:11:53 +0700

    Update line.py

--- route/line.py
+++ route/line.py
@@ -24,6 +24,7 @@ from linebot.v3.webhooks import (
 )
 
 from bson import ObjectId
+from urllib.parse import urlencode
 
 from models.models import User, express,lineUser
 from schemas.schemas import user_serializer, users_serializer, exPress_serializer, express_serializer, userToken_serializer, userTokens_serializer
@@ -80,42 +81,58 @@ async def push(to: str, messages: list[dict]):
             "https://api.line.me/v2/bot/message/push", headers=headers, json=body
         )
         print(f"status = {response.status_code}")
-        
 
-
-# แบบเก่าที่ทำครั้งแรก
-@line.post("/callback")
-async def handle_callback(request: Request):
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = await request.body()
-    body = body.decode()
-
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        raise HTTPException(status_code=400, detail="Invalid signature")
-
-
-    for data in collection_image.find():
-        # เช็ค status ว่า line มีการแจ้งเตือนหรือยัง
-        if data["status"] == False:
-
-
-
-# ตรงนี้เป็นการวนลูป event ในการตอบกลับผู้ใช้
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessageContent):
-            continue
-
-        await line_bot_api.reply_message(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
+@line.post("/verify_token")
+async def verify(id_token:str, name: str):
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    params = {
+        'id_token': id_token,
+        'client_id': '2004090496'
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "https://api.line.me/oauth2/v2.1/verify", headers=headers, params=urlencode(params)
         )
+        json_response = response.json()
+        sub = json_response.get('sub')
+        total = collection_line.insert_one(sub, name)
+        return total
+    
+@line.post("/id_token", tags=["token"])
+async def post_users(data: lineUser):
+    collection_line.insert_one(dict(data))
+    return {"status": "OK", "data":userTokens_serializer(collection_line.find())}
 
-    return 'OK'
+@line.get("/token", tags=["token"])
+async def get_token():
+    token = userTokens_serializer(collection_line.find())
+    return {"status":"ok", "data":token}
+
+
+class FollowEvent(BaseModel):
+    type: str
+    source: dict
+
+
+@line.get("/get_all_data")
+async def get_all_data():
+    users = users_serializer(collection_image.find())
+    return {"status":"ok", "data":users}
+
+@line.post("/express")
+async def post_express(data:express):
+    collection_express.insert_one(dict(data))
+    return {"status":"ok", "data":exPress_serializer(collection_express.find())}
+
+@line.get("/getdetail")
+async def get_detail():
+    detail = exPress_serializer(collection_express.find())
+    return {"status":"ok", "data":detail}
+
+@line.post("/post", tags=["user"])
+async def post_users(user: lineUser):
+    collection_line.insert_one(dict(user))
+    return {"status": "OK"}
+
