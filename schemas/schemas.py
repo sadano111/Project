6b9ddd62369016a339afb08a4bc9883dcbe1bbbe
sadano111 <101854082+sadano111@users.noreply.github.com>
2024-04-18 06:51:00 +0700

    Update schemas.py

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,4 +1,18 @@
 # ข้อมูลสำหรับผู้รับพัสดุ
+def ocr_serializer(item) -> dict:
+    return {
+        "id": str(item["_id"]),
+        "number": item["number"],
+        "phone": item["phone"],
+        "name": item["name"],
+        "company": item["company"],
+        "status": item["status"],
+        "take": item["take"]
+    }
+
+def ocrs_serializer(items) -> list:
+    return [ocr_serializer(item) for item in items]
+
 def user_serializer(user) -> dict:
     return {
         "id": str(user["_id"]),
@@ -13,20 +27,7 @@ def user_serializer(user) -> dict:
 def users_serializer(users) -> list:
     return [user_serializer(user) for user in users]
 
-# ข้อมูลสำหรับผู้ส่งพัสดุ
-def express_serializer(item) -> dict:
-    return{
-        "id": str(item["_id"]),
-        "name": item["name"],
-        "phone": item["phone"],
-        "express": item["express"],
-        "parcel": item["parcel"]
-    }
-
-def exPress_serializer(items) -> list:
-    return [express_serializer(item) for item in items]
-
-# get ผู้รับ
+# get ผู้รับ (นักศึกษา) login with line
 def parcel_serializer(item) -> dict:
     return{
         "id": str(item["_id"]),
@@ -60,16 +61,28 @@ def loginUser_serializer(item) -> dict:
 def loginUsers_serializer(items) -> list:
     return [loginUser_serializer(item) for item in items]
 
-def ocr_serializer(item) -> dict:
-    return {
+# ข้อมูลสำหรับผู้ส่งพัสดุ
+def express_serializer(item) -> dict:
+    return{
         "id": str(item["_id"]),
-        "number": item["number"],
-        "phone": item["phone"],
         "name": item["name"],
-        "company": item["company"],
-        "status": item["status"],
-        "take": item["take"]
+        "phone": item["phone"],
+        "express": item["express"],
+        "parcel": item["parcel"]
     }
 
-def ocrs_serializer(items) -> list:
-    return [ocr_serializer(item) for item in items]
+def exPress_serializer(items) -> list:
+    return [express_serializer(item) for item in items]
+
+def tableExpress_serializer(item) -> dict:
+    return {
+        "id": str(item["_id"]),
+        "date": item["date"],
+        "name": item["name"],
+        "phone": item["phone"],
+        "express": item["express"],
+        "parcel": item["parcel"]
+    }
+
+def tableExpresss_serializer(items) -> list:
+    return [tableExpress_serializer(item) for item in items]
